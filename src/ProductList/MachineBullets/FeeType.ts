c5fe56b5c9fa25c1b7a2a838e4eb4abd3ea1a3e7
Lucaspaula6 <lucas.paula@rethink.dev>
2022-11-07 16:48:46 -0300

    Feetype.ts => Forma de busca sem switch case.

--- src/ProductList/MachineBullets/FeeType.ts
+++ src/ProductList/MachineBullets/FeeType.ts
@@ -3,18 +3,38 @@ import { insert } from "./Inserter";
 export const verifyFeeType = (item: any, order: any, created: any[]) => {
   const feeType = item.fee?.type;
   if (!feeType) return created;
+
+  return insert(
+    feeInsertionArray[feeTypesArray.indexOf(feeType)],
+    order,
+    created,
+    item
+  );
+
   switch (feeType) {
     case "BOARDING":
-      return insert("BOARDING",  order, created, item);
+      return insert("BOARDING", order, created, item);
     case "CANCELLATION":
-      return insert("CANCEL_FLIGHT",  order, created, item);
+      return insert("CANCEL_FLIGHT", order, created, item);
     case "CONVENIENCE":
-      return insert("CONVENIENCE_TAX",  order, created, item);
+      return insert("CONVENIENCE_TAX", order, created, item);
     case "NO_MILES_BOOKING":
-      return insert("NO_MILES_BOOKING",  order, created, item);
+      return insert("NO_MILES_BOOKING", order, created, item);
     case "RESERVATION":
-      return insert("RESERVATION",  order, created, item);
-
+      return insert("RESERVATION", order, created, item);
   }
 };
-
+const feeTypesArray = [
+  "BOARDING",
+  "CANCELLATION",
+  "CONVENIENCE",
+  "NO_MILES_BOOKING",
+  "RESERVATION",
+];
+const feeInsertionArray = [
+  "BOARDING",
+  "CANCEL_FLIGHT",
+  "CONVENIENCE_TAX",
+  "NO_MILES_BOOKING",
+  "RESERVATION",
+];
